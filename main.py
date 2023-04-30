c3bbec57d0b87a6d540b2a6b039d1b572b426d7d
Bruno Barros <contato4code@gmail.com>
2023-04-30 18:34:39 -0300

    - Mostrando cores no display e na surface

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ pygame.init()
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Meteor Shooter")
+
+test_surf = pygame.Surface((400, 100))
 
 while True:
     # input -> events (click, movement, buttons, controls)
@@ -12,6 +15,7 @@ while True:
             pygame.quit()
             sys.exit()
     # updates
-
+    display_surface.blit(test_surf, (0, 0))
+    test_surf.fill((15, 140, 122))
     # show frames to player
     pygame.display.update()
