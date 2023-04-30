dd7977ee86473174b20c822a0b5e239432b10320
Bruno Barros <contato4code@gmail.com>
2023-04-30 18:07:58 -0300

    - First screen and close button added

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-import pygame
+import pygame, sys
+
+pygame.init()
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+while True:
+    # input -> events (click, movement, buttons, controls)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    # updates
+
+    # show frames to player
+    pygame.display.update()
