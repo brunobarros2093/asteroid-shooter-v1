32129538b9fc6b05195b4f358af763f6af0a9e39
Bruno Barros <brunog.barros@pm.me>
2023-05-02 21:18:29 -0300

    Displaying time onde estava o score [wip]

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ def laser_update(laser_list: list, speed: int = 300) -> None:
             laser_list.remove(rect)
 
 
+def display_score() -> None:
+    score_text = f'Score:{pygame.time.get_ticks()} '
+    text_surf = font.render(score_text, True, 'White')
+    text_rect = text_surf.get_rect(midbottom=(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 80))
+    display_surface.blit(text_surf, text_rect)
+    pygame.draw.rect(display_surface, 'White', text_rect.inflate(30, 30), width=8, border_radius=5)
+
+
 # game init
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
@@ -27,11 +35,9 @@ laser_surface = pygame.image.load('./graphics/laser.png').convert_alpha()
 
 # import text
 font = pygame.font.Font('./graphics/subatomic.ttf', 50)
-text_surf = font.render('Space', True, 'White')
 
 # rectangules
 ship_rect = ship_surface.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-text_rect = text_surf.get_rect(midbottom=(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 80))
 
 # laser_rect = laser_surface.get_rect(midbottom=ship_rect.midtop)
 laser_list = []
@@ -52,16 +58,15 @@ while True:
     ship_rect.center = pygame.mouse.get_pos()
 
     laser_update(laser_list)
+    pygame.time.get_ticks()
     # drawing
     display_surface.fill((0, 0, 0))
     display_surface.blit(background_surface, (0, 0))
-    display_surface.blit(text_surf, text_rect)
+
+    display_score()
+
     for rect in laser_list:
         display_surface.blit(laser_surface, rect)
-
-    # tests draw
-    pygame.draw.rect(display_surface, 'White', text_rect.inflate(30, 30), width=8, border_radius=5)
-    # end draw native rectangules
 
     display_surface.blit(ship_surface, ship_rect)
     # show frames to player - final frame
