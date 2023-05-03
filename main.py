11283702524bc71b4eaa1e366f1aff7128660956
Bruno Barros <brunog.barros@pm.me>
2023-05-03 20:15:10 -0300

    Lógica dos tiros, adicionando um timer limitador

--- main.py
+++ main.py
@@ -11,11 +11,19 @@ def laser_update(laser_list: list, speed: int = 300) -> None:
 
 
 def display_score() -> None:
-    score_text = f'Score:{pygame.time.get_ticks()} '
+    score_text = f'Score:{pygame.time.get_ticks() // 1000} '
     text_surf = font.render(score_text, True, 'White')
     text_rect = text_surf.get_rect(midbottom=(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 80))
     display_surface.blit(text_surf, text_rect)
     pygame.draw.rect(display_surface, 'White', text_rect.inflate(30, 30), width=8, border_radius=5)
+
+
+def laser_timer(can_shoot: bool, duration: int = 500) -> bool:
+    if not can_shoot:
+        current_time = pygame.time.get_ticks()
+        if current_time - shoot_time > duration:
+            can_shoot = True
+    return can_shoot
 
 
 # game init
@@ -26,6 +34,8 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("Meteor Shooter")
 # game init end
 
+can_shoot = True
+shoot_time = None
 # importing images
 ship_surface = pygame.image.load('./graphics/ship.png').convert_alpha()
 background_surface = pygame.image.load('./graphics/background.png').convert()
@@ -47,9 +57,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and can_shoot:
+            # laser
             laser_rect = laser_surface.get_rect(midbottom=ship_rect.midtop)
             laser_list.append(laser_rect)
+
+            # timer logic
+            can_shoot = False
+            shoot_time = pygame.time.get_ticks()
 
     # framerate limiter
     dt = clock.tick(120) / 1000  # 1000ms in 1s
@@ -58,6 +73,8 @@ while True:
     ship_rect.center = pygame.mouse.get_pos()
 
     laser_update(laser_list)
+    # laser timer
+    can_shoot = laser_timer(can_shoot, duration=400)
     pygame.time.get_ticks()
     # drawing
     display_surface.fill((0, 0, 0))
